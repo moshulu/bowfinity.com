47fc63e828e229541e69ceb4c1b730769d9acd02
moshulu <mdrew205@gmail.com>
2018-12-22 13:36:03 -0500

    Delete shopAction.php

--- shopAction.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	array_push($_SESSION["cart"], array($_POST["picture"],$_POST["name"],$_POST["qty"],$_POST["price"]));
-	print_r($_SESSION["cart"]);
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-?>
